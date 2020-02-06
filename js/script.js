02283b6d2ddf918548ae8c4b940ab46722ee9991
Krim Khalifa <57441308+k-khalifa@users.noreply.github.com>
2020-02-06 13:16:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,9 @@ $(function () {
 
     'use strict';
 
-    $(".login").height($(window).height()-50);
+    $(".bg-login").height($(window).height());
  
 
-    $(window).on("resize", function () {
-        $(".login").height($(window).height()-50);
-     
-    });
 
 
 
